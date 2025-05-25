26bab07a500abfc8f81f7b0a78f4b23245a33943
parth506 <parthagrawal4675@gmail.com>
2025-05-25 17:25:07 +0530

    string commit

--- JAVASCRIPT/vedio60/index.js
+++ JAVASCRIPT/vedio60/index.js
@@ -15,3 +15,24 @@ console.log("His anme is " + a + " and his firent nme is " + friend)
 console.log(`His name is ${a} and his friends name is ${friend}`)
 
 //escape sequence characters 
+let c = "Roh\"an"
+console.log(c)
+
+//Toggle case
+let b = "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+
+a = " Parth"
+//string slices 
+console.log(b.slice(1, 4))
+console.log(b.slice(1))
+console.log(b.replace("Sh", "hS"))
+console.log(b.concat(a))
+console.log(b.concat(a, "Agrawal", "PAri", "New"
+))
+
+let f = "  PArth   "
+console.log(f.trim()) //removes whitespaces 
+
