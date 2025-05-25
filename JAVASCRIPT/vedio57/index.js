5ae1a0939340bb9b987e3d115f9627c6d53f2e7c
parth506 <parthagrawal4675@gmail.com>
2025-05-25 12:39:50 +0530

    1.commit

--- JAVASCRIPT/vedio57/index.js
+++ JAVASCRIPT/vedio57/index.js
@@ -1,9 +1,22 @@
 
-console.log("Tutorial for loops ");
-console.log("Tutorial for loops ");
+// console.log("Tutorial for loops ");
+// console.log("Tutorial for loops ");
 
 
-let a = 4;
-for (let i = 0; i < 10; ++i) {
-    console.log(a + i);
+// let a = 4;
+// for (let i = 0; i < 10; ++i) {
+//     console.log(a + i);
+// }
+
+//For- in loop
+let obj = {
+    name: "Parth",
+    role: " Pogrammer ",
+    company: "Goldman Sachs"
+}
+
+
+for (const key in obj) {
+    const elemeny = obj[key];
+    console.log(key, elemeny);
 }
