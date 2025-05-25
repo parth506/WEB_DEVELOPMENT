ca38b6ed31ab0a06eae69606ec07f7fac7cbc7ce
parth506 <parthagrawal4675@gmail.com>
2025-05-25 12:22:54 +0530

    code changesd

--- JAVASCRIPT/vedio56/index.js
+++ JAVASCRIPT/vedio56/index.js
@@ -1,9 +1,14 @@
-console.log(" Hello this is cnsole")
+// console.log(" Hello this is cnsole")
 
-let age=45;
-if(age>18){
-    console.log("18")
+let age = 45;
+if (age > 18) {
+    console.log("18");
 }
-else{
-    console.log("e6")
+else {
+    console.log("e6");
 }
+
+// let a = 2;
+// let b = 2;
+// a+=b;
+// console.log(a);
