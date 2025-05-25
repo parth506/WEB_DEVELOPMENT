67f214494704e9d11f12f866bf6ed05d78153c38
parth506 <parthagrawal4675@gmail.com>
2025-05-25 17:16:14 +0530

    new changes

---
+++ JAVASCRIPT/vedio60/index.js
@@ -0,0 +1,17 @@
+let a = "Harry";
+console.log(a.length);
+console.log(a[0])
+console.log(a[1])
+
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+let friend = "Parth"
+console.log("His anme is " + a + " and his firent nme is " + friend)
+
+//template literals
+//using backtic
+console.log(`His name is ${a} and his friends name is ${friend}`)
+
+//escape sequence characters 
